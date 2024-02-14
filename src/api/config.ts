c01db994452660190444e6f0ba99e8c3c7c87262
lst97 <laisiotou1997@gmail.com>
2024-02-14 15:42:25 +1100

    update: incorrect protocol

--- src/api/config.ts
+++ src/api/config.ts
@@ -2,7 +2,7 @@ export const API_VERSION = 'v1';
 export const PORT = 1168;
 export const HOST = 'lst97.tplinkdns.com';
 export const API_ENDPOINT = `/api/${API_VERSION}`;
-export const API_BASE_URL = `http://${HOST}:${PORT}${API_ENDPOINT}`;
+export const API_BASE_URL = `https://${HOST}:${PORT}${API_ENDPOINT}`;
 
 export const API_ENDPOINTS = {
     fetchStaffsData: `${API_BASE_URL}/staffs`,
